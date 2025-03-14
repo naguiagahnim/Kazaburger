c0480e65fa53aa782f29a57beb0f91ebdae6ff7e
Agahnim <43116178+naguiagahnim@users.noreply.github.com>
2025-03-14 14:25:11 +0100

    fix nique

--- backend/router.js
+++ backend/router.js
@@ -1,19 +1,12 @@
 const express = require("express");
 const router = express.Router();
 module.exports = router;
-
-router.get("/", async(req, res) => {
-    console.log("Tous les produits")
+router.get("/", async(req, res) => {console.log("tous les produits");});
+router.get("/:val", async(req, res) => { console.log("un produit par id :val");});
+router.use((req, res) => {
+res.status(404);
+res.json({
+error: `${req.method + ":" + req.originalUrl} Page not found`,
 });
-
-router.get("/:val", async(req, res) => {
-    console.log("Un produit par id : val")
 });
-
-router.get((req, res) => {
-    res.status(404);
-    res.json({error: `${req.method + " : " + req.originalUrl} Page not found`,
-    });
-});
-
 module.exports = router;
