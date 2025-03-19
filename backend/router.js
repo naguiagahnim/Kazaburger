23e9416b8f72b7dc30323a92f4b533c77956b82c
Agahnim <43116178+naguiagahnim@users.noreply.github.com>
2025-03-19 17:07:09 +0100

    add routes produit

--- backend/router.js
+++ backend/router.js
@@ -1,8 +1,39 @@
+const url = process.env.KAZABURGER_API_URL;
+const token = process.env.KAZABURGER_API_TOKEN;
 const express = require("express");
 const router = express.Router();
 module.exports = router;
-router.get("/product/", async(req, res) => {res.send(req.query);});
-router.get("/product/:id", async(req, res) => {res.send("un produit par id : " + `${req.params.id}`);});
+
+router.get("/product/:id", async(req, res) => {;
+    fetch(url + "/product/" + req.params.id, {
+    method: 'GET',
+    })
+    .then(response => {
+    if (response.ok) {        
+        return response.json().then((data) =>
+            {res.json(data);}) 
+    }
+    else{
+        throw new Error('Là = y a un problème :');
+    }
+    })
+
+});
+router.get("/product/", async(req, res) => {
+const fetchurl = req.query.family ? url + "/product/?family=" + req.query.family : req.query.title ? url + "/product/?title=" + req.query.title : url + "/product/";
+fetch(fetchurl, {
+    method: 'GET', 
+    })
+    .then(response => {
+    if (response.ok) {        
+        return response.json().then((data) =>
+            {res.json(data);})
+    }
+    else{
+        throw new Error('Là = y a un problème :');
+    }
+    })
+});
 
 router.get("/testimony", async(req, res) => {res.send(req.query);});
 router.get("/testimony/:id", async(req, res) => {res.send("un témoignage par id : " + `${req.params.id}`);});
